419f0e2c3445ff26caf61c04d34237e4f94f106b
Bibhor07 <bibhor.acharya16@gmail.com>
2024-03-05 15:24:16 -0600

    Added requirements.txt generated by pip

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from edgar_functions import get_same_sic_companies
+from edgar_functions import get_same_sic_companies, generate_main_df
+import streamlit.components.v1 as components
 
 # Load the data
 df = pd.read_csv("Final_company_data.csv")
@@ -9,15 +10,25 @@ df = pd.read_csv("Final_company_data.csv")
 companies_data = [
     {"Company": "Hershey Co.", "Ticker": "HSY", "Cik": "0000047111"},
     {"Company": "Walmart", "Ticker": "WMT", "Cik": "0000104169"},
-    # Add other companies here
+    {"Company": "Paccar", "Ticker": "PCAR", "Cik": "0000075362"},
+    {"Company": "Sysco Corp.", "Ticker": "SYY", "Cik": "0000096021"},
+    {"Company": "Tesla", "Ticker": "TSLA", "Cik": "0001318605"},
+    {"Company": "General Motors", "Ticker": "GM", "Cik": "0001467858"},
+    {"Company": "Target Corp", "Ticker": "TGT", "Cik": "0000027419"},
+    {"Company": "Kraft Inc.", "Ticker": "KHC", "Cik": "0001637459"},
+    {"Company": "The Clorox Company", "Ticker": "CLX", "Cik": "0000021076"},
+    {"Company": "Amazon Inc.", "Ticker": "AMZN", "Cik": "0001018724"},
+    {"Company": "Nike Inc.", "Ticker": "NKE", "Cik": "0000320187"},
+    {"Company": "Chipotle Mexican Grill Inc.", "Ticker": "CMG", "Cik": "0001058090"},
+    {"Company": "McDonald's Inc", "Ticker": "MCD", "Cik": "0000063908"},
+    {"Company": "Lululemon Athletics Inc. (LULU)", "Ticker": "LULU", "Cik": "0001397187"},
+    {"Company": "Coca Cola", "Ticker": "KO", "Cik": "0000021344"},
+    {"Company": "Molson Coors", "Ticker": "TAP", "Cik": "0000024545"}
 ]
-
-
 # Function to get or create session state
 def get_session_state():
     if 'selected_similar_companies' not in st.session_state:
-        st.session_state.selected_similar_companies = []
-
+        st.session_state.selected_similar_companies = set()
 
 # First Page
 if 'page' not in st.session_state:
@@ -58,7 +69,14 @@ elif st.session_state.page == 2:
                                                n_accessions=st.session_state.num_fiscal_years,
                                                n_companies=st.session_state.num_similar_companies)
 
-    # Get or create session state for checkboxes
+    # Set similar companies in session state
+    st.session_state.similar_companies = similar_companies
+
+    # Set ticker of initially selected company
+    st.session_state.initial_selected_company_ticker = next(
+        company['Ticker'] for company in companies_data if company['Company'] == st.session_state.selected_company_name)
+
+    # Get or create session state for selected similar companies
     get_session_state()
 
     # Display similar companies as checkboxes
@@ -67,14 +85,12 @@ elif st.session_state.page == 2:
         checkbox_value = st.checkbox(f"{company[2]} ({company[0]})", value=(
                     f"{company[2]} ({company[0]})" in st.session_state.selected_similar_companies))
         if checkbox_value:
-            if f"{company[2]} ({company[0]})" not in st.session_state.selected_similar_companies:
-                st.session_state.selected_similar_companies.append(f"{company[2]} ({company[0]})")
+            st.session_state.selected_similar_companies.add(f"{company[2]} ({company[0]})")
         elif f"{company[2]} ({company[0]})" in st.session_state.selected_similar_companies:
             st.session_state.selected_similar_companies.remove(f"{company[2]} ({company[0]})")
 
     # Back button
     if st.button("Back"):
-        st.session_state.selected_similar_companies.clear()
         st.session_state.page = 1
 
     # Finalize Selection button
@@ -85,11 +101,70 @@ elif st.session_state.page == 2:
 elif st.session_state.page == 3:
     st.title('Finalized Selection')
 
+    # Retrieve similar companies from session state
+    similar_companies = st.session_state.similar_companies
+
     # Display selected similar companies
     st.write('Selected Companies are:')
     for company in st.session_state.selected_similar_companies:
         st.write('- ' + company)
 
+    # Get the CIKs and tickers of all selected similar companies
+    selected_ciks = []
+    selected_tickers = []
+    for company in similar_companies:
+        if f"{company[2]} ({company[0]})" in st.session_state.selected_similar_companies:
+            selected_ciks.append(company[1])
+            selected_tickers.append(company[0])
+
+    # Add CIK of initially selected company
+    selected_company_cik = next(
+        company['Cik'] for company in companies_data if company['Company'] == st.session_state.selected_company_name)
+    selected_ciks.append(selected_company_cik)
+    selected_tickers.append(st.session_state.initial_selected_company_ticker)
+
+    # Print selected CIKs
+    st.write('Selected CIKs are:')
+    for cik in selected_ciks:
+        st.write('- ' + cik)
+
+    # Print selected tickers
+    st.write('Selected Tickers are:')
+    for ticker in selected_tickers:
+        st.write('- ' + ticker)
+
+    # Store fys and selected_tickers in session state
+    st.session_state.fys = [2023 - i for i in range(st.session_state.num_fiscal_years)]
+    st.session_state.selected_tickers = selected_tickers
+
+    # Create a list of fiscal years
+    fys = st.session_state.fys
+
+    # Display the list of fiscal years
+    st.write('Fiscal Years:', fys)
+
+    # Next button
+    if st.button("Next"):
+        st.session_state.page = 4
+
+# Get Data Page
+elif st.session_state.page == 4:
+    st.title('Get Data')
+
+    # Button to get data
+    if st.button("Get Data"):
+        # Get the data
+        main_df = generate_main_df(st.session_state.fys, st.session_state.selected_tickers)
+
+        # Display the data as a Streamlit DataFrame
+        st.write(main_df)
+
+        # Print message indicating data is ready
+        st.write("Your data is ready to download.")
+
+
     # Back button
     if st.button("Back"):
-        st.session_state.page = 2
+        st.session_state.page = 3
+
+
--- main.py
+++ main.py
@@ -21,9 +21,82 @@ companies_data = [
     {"Company": "Molson Coors", "Ticker": "TAP", "Cik": "0000024545"}
 ]
 df = pd.read_csv("Final_company_data.csv")
-cik = int("0000047111")
-print(get_same_sic_companies(df, cik = cik))
+ticker = "WMT"
+cik = cik_matching_ticker(ticker)
+int_cik = int(cik)
 
+similar_companies = get_same_sic_companies(df, cik = int_cik)
+
+selected_tickers = [x[0] for x in similar_companies]
+selected_tickers.append(ticker)
+
+print(selected_tickers)
+
+fys = [2023,2022,2021,2020]
+main_df = pd.DataFrame()
+for ticker in selected_tickers:
+    company_facts = {
+        "Total assets": {"company_fact": "Assets", "format": "us-gaap"},
+        "Total liabilities": {"company_fact": "Liabilities", "format": "us-gaap"},
+        "Retained earnings": {"company_fact": "RetainedEarningsAccumulatedDeficit", "format": "us-gaap"},
+        "Operating Income": {"company_fact": "OperatingIncomeLoss", "format": "us-gaap"},
+        "Total stockholders' equity": {
+            "company_fact": ["StockholdersEquityIncludingPortionAttributableToNoncontrollingInterest",
+                             "StockholdersEquity"], "format": "us-gaap"},
+        "Net income attributable to the shareholders": {"company_fact": "NetIncomeLoss", "format": "us-gaap"},
+        "Net income per share": {"company_fact": "EarningsPerShareBasic", "format": "us-gaap"},
+        "Dividend per share": {"company_fact": "CommonStockDividendsPerShareDeclared", "format": "us-gaap"},
+        "number of share outstanding": {"company_fact": "CommonStockSharesOutstanding", "format": "us-gaap"}
+    }
+    cik = cik_matching_ticker(ticker)
+    response = requests.get(f"https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json", headers=headers)
+    data = response.json()['facts']
+    company_facts_update = get_label_calc_tags(ticker)['assigned_tags']
+    company_facts.update(company_facts_update)
+    company_df = pd.DataFrame()
+    for fact_name, tag_details in company_facts.items():
+        tag = tag_details['company_fact']
+        format = tag_details['format']
+        list_of_tags = data[format].keys()
+        values = []
+        tag_in_taglist = False
+        if type(tag) == type([]):
+            for t in tag:
+                if t in list_of_tags:
+                    tag_in_taglist = True
+                    tag = t
+                    break;
+        if type(tag) == type(""):
+            if tag in list_of_tags:
+                tag_in_taglist = True
+        if tag_in_taglist:
+            key = list(data[format][tag]['units'].keys())[0]
+            for fy in fys:
+                available = False
+                for info in data[format][tag]['units'][key]:
+                    if info['fy'] == fy and info['form'] == '10-K':
+                        available = True
+                        val = info['val']
+                if available is False:
+                    val = 'N/A'
+                values.append(val)
+        else:
+            values = ["N/A"] * len(fys)
+
+        temp = pd.DataFrame({"FY": fys, "Value": values})
+        temp['Fact'] = fact_name
+        if len(company_df) == 0:
+            company_df = temp
+        else:
+            company_df = pd.concat([company_df, temp])
+    company_df['Ticker'] = ticker
+    company_df = company_df[['Ticker', 'Fact', 'Value', 'FY']]
+    if len(main_df) == 0:
+        main_df = company_df
+    else:
+        main_df = pd.concat([main_df, company_df])
+
+print(main_df)
 
 
 
